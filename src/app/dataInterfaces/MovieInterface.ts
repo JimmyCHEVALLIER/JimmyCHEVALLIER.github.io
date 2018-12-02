85d0446a9b9a524c8352c96c720cd361ac1b1d29
s181813 <jimmy.chevallier@supinfo.com>
2018-12-02 17:15:03 +0100

    remake

--- src/app/dataInterfaces/MovieInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface MovieInterface {
-  movieid: string; // name
-  keyword: string; // capital
-  moviename: string; // capital
-  releasedate: string; // alpha3Code
-  budget: string; // region
-  boxoffice: string; // borders
-}
